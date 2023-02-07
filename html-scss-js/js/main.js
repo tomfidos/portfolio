1be7149aeb1f0d0cf6540b2386f61ec77a3cbf40
Tomasz Fidos <fidostomasz@gmail.com>
2023-02-07 15:29:14 +0100

    Added mobile controls open & hide handling.

---
+++ html-scss-js/js/main.js
@@ -0,0 +1,9 @@
+$('.hamburger').click(function() {
+    $('.menu').toggleClass('open');
+    $(this).toggleClass('hide');
+});
+
+$('.menu-close').click(function() {
+    $('.menu').toggleClass('open');
+    $('.hamburger').toggleClass('hide');
+});
